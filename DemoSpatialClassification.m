0ad5514ebc33cf650cf99664d0db977580075daf
galad-loth <jlfeng911@gmail.com>
2016-10-29 15:42:59 +0800

    Add files via upload

--- DemoSpatialClassification.m
+++ DemoSpatialClassification.m
@@ -4,7 +4,7 @@ addpath(genpath(pwd))
 %% 
 disp('Loading data.');
 dataInfo=HsiDataLoad('PaviaUniv');
-dataInfo.splitInfo=SplitData(dataInfo.gt_data,'fix_num',300,true);
+dataInfo.splitInfo=SplitData(dataInfo.gt_data,'fix_num',100,true);
 
 [nr,nc]=size(dataInfo.gt_data);
 train_feat=VectorIndexing3D(dataInfo.spectral_data,dataInfo.splitInfo.train_idx);
@@ -20,7 +20,7 @@ reduce_dim=32;
 [train_feat,pca_eigvec,pca_eigval]=MyPCA(train_feat,1,reduce_dim);
 test_feat=MyPCA(test_feat,2,pca_eigvec);
 pca_feat=reshape(test_feat, [nr nc reduce_dim]);
-[sp_label, num_sp] = slicomex(pca_feat,5000);
+[sp_label, num_sp] = mexSLIC(pca_feat,4000,20,5);
 %% 
 disp('Perform classification.');
 % [w,val_loss] = MLRTrainAL(train_feat',train_label', 0.1,0.0001,100);
@@ -40,13 +40,13 @@ test_grammat=[(1:size(test_grammat,1))',test_grammat];
 pred_label_2d=reshape(pred_label,[nr nc]);
 pred_label_mv=SpatialMajorityVoting(pred_label_2d, sp_label);
 %%
-idx_gt_pixel=dataInfo.gt_data>0;
-temp=zeros(nr,nc);
-temp(idx_gt_pixel)=pred_label_2d(idx_gt_pixel);
-pred_label_2d=temp;
-temp=zeros(nr,nc);
-temp(idx_gt_pixel)=pred_label_mv(idx_gt_pixel);
-pred_label_mv=temp;
+% idx_gt_pixel=dataInfo.gt_data>0;
+% temp=zeros(nr,nc);
+% temp(idx_gt_pixel)=pred_label_2d(idx_gt_pixel);
+% pred_label_2d=temp;
+% temp=zeros(nr,nc);
+% temp(idx_gt_pixel)=pred_label_mv(idx_gt_pixel);
+% pred_label_mv=temp;
 
 [clsStat,mat_conf]=GetAccuracy(dataInfo.splitInfo.test_label,pred_label(dataInfo.splitInfo.test_idx));
 disp(['Overall Accuracy:',num2str(clsStat.OA),', Kappa Coeffcient:', num2str(clsStat.Kappa)]);
